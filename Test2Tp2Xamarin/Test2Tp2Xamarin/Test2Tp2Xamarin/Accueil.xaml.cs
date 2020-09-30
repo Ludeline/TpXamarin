d99cf6544bac475ae4dab11efeff0cc3aa332f58
Lucille <l.hochard.pro@gmail.com>
2020-09-30 09:29:31 +0200

    Tp2

---
+++ Test2Tp2Xamarin/Test2Tp2Xamarin/Test2Tp2Xamarin/Accueil.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test2Tp2Xamarin
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Accueil : ContentView
+    {
+        public Accueil()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Test2Tp2Xamarin/Test2Tp2Xamarin/Test2Tp2Xamarin/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Test2Tp2Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+    }
+}
