eb86a93d5995c5ffda0f6db22e1c1fa04d5aec0b
Lucille <l.hochard.pro@gmail.com>
2020-09-30 11:39:35 +0200

    Premier jet apres MAJ VS

---
+++ Test8242Tp3Xamarin/Test8242Tp3Xamarin/Test8242Tp3Xamarin/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Test8242Tp3Xamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            this.Connexion.Clicked += Connexion_Clicked;
+        }
+
+        private void Connexion_Clicked(object sender, EventArgs e)
+        {
+
+            if(this.Mail.Text.Length < 3 && this.Mail.Text != null)
+            {
+                this.Errors.IsVisible = true;
+                this.Errors.Text = "Votre mail n'est pas bon.";
+            }
+
+            if(this.Password.Text.Length < 6 && this.Password.Text != null)
+            {
+                this.Errors.IsVisible = true;
+                this.Errors.Text = "Votre MDP n'est pas bon.";
+            }
+
+            if(this.Mail.Text.Length >= 3 && this.Password.Text.Length >= 6)
+            {
+                this.Vue.IsVisible = true;
+            }
+        }
+    }
+}
